a5c425c0bfa96291383f8317a829a88bb3312c6c
Alex Luo <aluo50@stuy.edu>
2024-05-22 09:57:48 -0400

    added structure of Game class and overview/group info in README

---
+++ Game.java
@@ -0,0 +1,27 @@
+public class Game{
+    private Map map;
+    private Tower[] towers;
+    private Bloon[] bloons;
+    private int currency;
+    private int health;
+
+    public void startGame(){
+
+    }
+    public void update(){
+
+    }
+    public void placeTower(String towerName, int x, int y){
+
+    }
+    public Tower selectTower(int x, int y){
+        
+    }
+    public void sellTower(Tower towerName, int x, int y){
+
+    }
+    public void towerUpgrade(Tower tower){
+        
+    }
+
+}
--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 # APCS2 Final Project
 THIS DOCUMENT IS REQUIRED
 ## Group Info
+
+Period 3
+Alex Luo, Alan Chen 
+Group Name : Blooners
+
 ## Overview
+
+Description: A small recreation of Bloons Tower Defense, including one map, several towers with 2 upgrade paths, and Bloon movement.
+
 ## Instructions
