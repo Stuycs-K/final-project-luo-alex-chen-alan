3f16407606ab7b59d695b9095ae4816abbae72c5
Alex Luo <aluo50@stuy.edu>
2024-05-22 10:06:14 -0400

    added map class structure

---
+++ Map.java
@@ -0,0 +1,21 @@
+public class Map{
+    public PVector[] pathWaypointPositionList;
+    public PVector[] towerPositions;
+    public int pathWidth;
+
+    public PVector getNextPosition(int x, int y){
+
+    }
+
+    public boolean isOnPath(PVector position){
+
+    }
+
+    public boolean isInTowerBounds(PVector towerPosition){
+
+    }
+    
+    public boolean canPlace(int x, int y){
+        
+    }
+}
