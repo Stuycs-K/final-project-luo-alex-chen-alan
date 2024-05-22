11e7addc24704cb3127b77b203b6fb6ec21a5293
Alex Luo <aluo50@stuy.edu>
2024-05-22 10:14:20 -0400

    added tower class structure

---
+++ Tower.java
@@ -0,0 +1,23 @@
+public class Tower{
+    private int x;
+    private int y;
+    private int damage;
+    private int range;
+    private int attackSpeed;
+
+    public void attack(Bloon bloons){
+
+    }
+
+    public void upgrade(){
+
+    }
+
+    public int sell(){
+
+    }
+
+    public void place(int x, int y){
+        
+    }
+}
